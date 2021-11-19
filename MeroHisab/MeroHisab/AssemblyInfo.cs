c56e72e9e0f4067c28aa024eff055643b95ae778
developer.niroj <developer.niroj@gmail.com>
2021-11-19 23:46:07 +0545

    initial

---
+++ MeroHisab/MeroHisab/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("FontAwesomeRegular.otf", Alias = "FontAwesomeRegular")]
+[assembly: ExportFont("FontAwesomeSolid.otf", Alias = "FontAwesomeSolid")] 
