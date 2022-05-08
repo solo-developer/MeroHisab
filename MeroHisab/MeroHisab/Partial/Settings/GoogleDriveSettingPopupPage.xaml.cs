239f87a8e483b9f14f2c205fc1865ab0869b5bfb
developer.niroj <developer.niroj@gmail.com>
2022-05-09 01:46:18 +0545

    google drive config

---
+++ MeroHisab/MeroHisab/Partial/Settings/GoogleDriveSettingPopupPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using MeroHisab.ViewModels;
+using Rg.Plugins.Popup.Pages;
+using Xamarin.Forms.Xaml;
+
+namespace MeroHisab.Partial.Settings
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GoogleDriveSettingPopupPage : PopupPage
+    {
+        public GoogleDriveSettingPopupPage()
+        {
+            InitializeComponent();
+            var vm = App.GetViewModel<GoogleDriveSyncViewModel>();
+            BindingContext = vm;
+        }
+    }
+}
---
+++ MeroHisab/MeroHisab/ViewModels/Profile/GoogleDriveSyncViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MeroHisab.ViewModels
+{
+    public class GoogleDriveSyncViewModel : ViewModelBase
+    {
+        public GoogleDriveSyncViewModel()
+        {
+
+        }
+    }
+}
