462374783e6a4095f5adc9a094c755d72e979326
developer.niroj <developer.niroj@gmail.com>
2022-05-10 02:58:35 +0545

    login page material design

---
+++ MeroHisab/MeroHisab/ViewModels/LoginPageModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.CommunityToolkit.ObjectModel;
+
+namespace MeroHisab.ViewModels
+{
+    public class LoginPageModel : ViewModelBase
+    {
+        public IAsyncCommand LoginButtonClickedCommand { get; set; }
+        public LoginPageModel()
+        {
+            LoginButtonClickedCommand = new AsyncCommand(Login,allowsMultipleExecutions:false);
+        }
+
+        private async Task Login()
+        {
+           await _navigationService.NavigateToAsync<MainPageModel>(); 
+        }
+    }
+}
